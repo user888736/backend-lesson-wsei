535d6ff3608b596b8d5adb34bef7c67d170531d7
Szymon Kwaśniewski <75159136+user888736@users.noreply.github.com>
2022-06-21 17:00:18 +0200

    Poprawki i pierwsze testy rejestracji.

--- projekt/library-management-app/src/main/java/com/example/demo/web/UserRegistrationController.java
+++ projekt/library-management-app/src/main/java/com/example/demo/web/UserRegistrationController.java
@@ -1,7 +1,9 @@
 package com.example.demo.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.service.UserService;
@@ -17,6 +19,17 @@ public class UserRegistrationController {
 		this.userService = userService;
 	}
 	
+	@ModelAttribute("user")
+	public UserRegistrationDto userRegistrationDto() {
+		return new UserRegistrationDto();
+	}
+	
+	@GetMapping
+	public String showRegistrationForm() {
+		return "registration";
+	}
+	
+	@PostMapping
 	public String registerUserAccount(@ModelAttribute("user") UserRegistrationDto registrationDto) {
 		userService.save(registrationDto);
 		return "redirect:/registration?success";
